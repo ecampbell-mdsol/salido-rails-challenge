9b9ffba01f05178de8940cfd15412b2144e21ccf
Ernie Campbell <ecampbell@mdsol.com>
2015-04-18 17:59:45 -0400

    Fixes failing spec after UI change.

--- spec/views/products/index.html.haml_spec.rb
+++ spec/views/products/index.html.haml_spec.rb
@@ -5,12 +5,14 @@ RSpec.describe 'products/index', type: :view do
     assign(:products, [
       Product.create!(
         name: 'Name',
+        endeca_id: 'ID',
         product_type: 'Type',
         price_retail: '99.99',
         description: 'Description'
       ),
       Product.create!(
         name: 'Name',
+        endeca_id: 'ID',
         product_type: 'Type',
         price_retail: '99.99',
         description: 'Description'
@@ -20,9 +22,9 @@ RSpec.describe 'products/index', type: :view do
 
   it 'renders a list of products' do
     render
-    assert_select "tr>td", text: 'Name', count: 2
-    assert_select "tr>td", text: 'Type', count: 2
-    assert_select "tr>td", text: '$99.99', count: 2
-    assert_select "tr>td", text: 'Description', count: 2
+    assert_select 'tr>td', text: 'Name', count: 2
+    assert_select 'tr>td', text: 'ID', count: 2
+    assert_select 'tr>td', text: 'Type', count: 2
+    assert_select 'tr>td', text: '$99.99', count: 2
   end
 end
