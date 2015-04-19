bdd7125cc8c53780a59aad8f76027218054380a1
Ernie Campbell <ecampbell@mdsol.com>
2015-04-19 19:08:08 -0400

    Seeds a single product to get developers started.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# coding: utf-8
+Product.find_or_create_by!(
+  name: 'Apple Corer',
+  product_type: 'Accessory',
+  price_retail: 0.99,
+  description: 'It can core an apple!',
+)
