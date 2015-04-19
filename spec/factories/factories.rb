045d106d9c58afb6989daa2a9da0e2ad82cb9eba
Ernie Campbell <ecampbell@mdsol.com>
2015-04-19 18:38:05 -0400

    Tighten up rspec coverage.

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :product do
-    created_at 'N/A'
     description 'Fruity'
     endeca_id '123'
     name 'Red Wine'
@@ -9,7 +8,6 @@ FactoryGirl.define do
     price_retail '9.99'
     product_type 'Wine'
     url 'http://www.example.com/red_wine.html'
-    updated_at 'N/A'
     year '2015'
   end
 end
